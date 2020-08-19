bb7801ee4fecf76ff185be13a020008f92c75686
Dmitrii_Priporov <Dmitrii_Priporov@epam.com>
2020-08-19 11:19:19 +0300

    used interface fix

--- ktor-eureka-client/src/main/kotlin/dev/priporov/eureka/client/config/EurekaConfig.kt
+++ ktor-eureka-client/src/main/kotlin/dev/priporov/eureka/client/config/EurekaConfig.kt
@@ -1,13 +1,13 @@
 package dev.priporov.eureka.client.config
 
-import com.netflix.discovery.EurekaClientConfig
 import dev.priporov.eureka.client.config.client.KtorClientConfig
+import dev.priporov.eureka.client.config.client.KtorEurekaClientConfig
 import dev.priporov.eureka.client.config.instance.KtorEurekaInstanceConfig
 import dev.priporov.eureka.client.config.instance.KtorInstanceConfig
 
 class EurekaConfig :
 
-    EurekaClientConfig by KtorClientConfig(),
+    KtorEurekaClientConfig by KtorClientConfig(),
     KtorEurekaInstanceConfig by KtorInstanceConfig() {
 
 }
